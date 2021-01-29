7e1bedf68f52c6c9c3214c5c23249af4f0c6a317
fnafaatSSU <72876690+fnafaatSSU@users.noreply.github.com>
2021-01-29 12:32:00 -0500

    Add files via upload

---
+++ Lab01.java
@@ -0,0 +1,10 @@
+class Lab01
+{ 
+   //Please print your name as a test for this file
+    public static void main(String args[]) 
+    { 
+        //1.Write a Java program to test if 2D array contains a specific value?
+        //2.Write a Java program to find the index of 2D array element? 
+
+    } 
+} 
